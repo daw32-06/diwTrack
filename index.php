174f201d621e914b344b69b0f2246af0603f3e8a
daw32-06 <admin@tallernt.es>
2017-02-16 13:52:34 +0100

    Añadida primera vista

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+    /**
+    *   @todo Enrutador principal que carga los controladores dependiendo de la sesion
+    *         o llama al controlador dependiendo del parametro "location" de la url
+    *   @author Juan José Rubio Iglesias
+    */
+
+    include_once("view/indexView.php");
+
+    // Iniciamos la sesion
+    //session_start();
+
+    // Incluimos la libreria que controla las sesiones
+    //include_once("controller/sessionController.php");
+
+    /**
+    * Comprobamos si la session esta iniciada llamando al SessionController::logueado
+    * si devuelve verdadero es que la sesion esta iniciada y nos carga el controlador
+    * principal o en funcion del parametro 'location'
+    **/
+
+    /*if(SessionController::logueado())
+    {
+        if(isset($_GET['location']))
+        {
+            // Concatenamos la la localizacion con Controller.php. por ejemplo loqueseaController.php
+
+            $location = $_GET['location']."Controller.php";
+            $controlador = "controller/$location";
+
+            // Logoff
+            if($_GET['location'] == "logoff")
+            {
+                echo "logoff";
+                sessionController::cerrarSesion();
+                $controlador = "controller/loginController.php";
+            }
+
+            // Comprobamos si NO existe el controlador existe el controlador principal sera el principal
+            if(!file_exists($controlador)){
+                $controlador = "controller/mainController.php";
+            }
+        }else{
+            // Si no se ha indicado ninguna localizacion seleccionara el controlador principal
+            $controlador = "controller/mainController.php";
+        }
+    }else{
+        // Si no hemos iniciado sesion indicamos que el controlador sera  el login
+        $controlador = "controller/loginController.php";
+    }
+
+    // Cargamos el controlador
+    include $controlador;
+    */
+?>
---
+++ view/indexView.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="webroot/css/indexView.min.css">
+    <title>DiwTrack</title>
+</head>
+<body>
+    <button>Iniciar Sesion</button><button>Registrate</button>
+    <div id="signin">
+        <form>
+            <input type="text" name="usuario">
+            <br>
+            <input type="text" name="password">
+            <div id="loginmsg"></div>
+            <button>ENTRAR</button>
+        </form>
+    </div>
+    <div id="signup">
+
+    </div>
+</body>
+</html>
