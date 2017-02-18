bba68bd6e69f73ceb1ecc1242151cb2364b60c43
Juan José Rubio Iglesias <admin@tallernt.es>
2017-02-18 12:53:52 +0100

    commit

--- view/indexView.php
+++ view/indexView.php
@@ -29,33 +29,58 @@
 </head>
 <body>
     <header>
-        <button class="signinBT" onclick="identificarse()">Identificate</button><button class="signupBT" onclick="registrarse()">Regístrate Gratis</button>
+        <span class="titulo">DiwTrack</span>
+        <span id="botonesHeader">    <button class="signinBT" onclick="identificarse()">Identificate</button><button class="signupBT" onclick="registrarse()">Regístrate Gratis</button></span>
     </header>
+
+
+
+
+
+    <!-- Ventanas de registro e inicio de sesion -->
     <div id="signin">
-        <form>
-            <div class="close" onclick="closeIdentificarse()"><img width="25" height="25" src="webroot/img/close_btn.png"></div>
-            <input type="text" name="usuario">
-            <br>
-            <input type="text" name="password">
-            <div id="loginmsg"></div>
-            <button>ENTRAR</button>
-        </form>
+        <div id="signinVentana">
+            <form>
+
+                <div class="close" onclick="closeIdentificarse()"><img width="25" height="25" src="webroot/img/close_btn.png"></div>
+                <div id="campos">
+                    <h1>Inicia sesion</h1>
+                    <div class="social">Redes sociales<br>
+
+                            <img src="https://facebookbrand.com/wp-content/themes/fb-branding/prj-fb-branding/assets/images/fb-art.png" alt="facebook" height="40px" width="40px">
+                        <img src="http://iconbug.com/data/2f/512/7090d90b6a06b0ca3d948bcea9e676f9.png" alt="google" height="40px" width="40px">
+                        <img src="https://edubloxtutor.com/wp-content/uploads/2016/11/twitter-3-xxl.png" alt="twitter" height="40px" width="40px">
+
+                    </div>
+
+                    O inicia sesion con tu usuario y contraseña
+                    <input type="text" placeholder="Usuario" name="usuario">
+
+                    <input type="text" placeholder="Password"name="password">
+                    <div id="loginmsg"></div>
+                    <button>ENTRAR</button>
+                </div>
+            </form>
+        </div>
     </div>
     <div id="signup">
-
-        <form>
-            <div class="close" onclick="closeRegistrarse()"><img width="25" height="25" src="webroot/img/close_btn.png"></div>
-            <input type="text" name="usuario">
-            <br>
-            <input type="text" name="password">
-                <input type="text" name="password">
+        <div id="signupVentana">
+            <form>
+                <div class="close" onclick="closeRegistrarse()"><img width="25" height="25" src="webroot/img/close_btn.png"></div>
+                <input type="text" name="usuario">
+                <br>
+                <div id="campos">
                     <input type="text" name="password">
-                        <input type="text" name="password">
-                            <input type="text" name="password">
-                                <input type="text" name="password">
-            <div id="loginmsg"></div>
-            <button>ENTRAR</button>
-        </form>
+                    <input type="text" name="password">
+                    <input type="text" name="password">
+                    <input type="text" name="password">
+                    <input type="text" name="password">
+                    <input type="text" name="password">
+                    <div id="loginmsg"></div>
+                    <button>ENTRAR</button>
+                </div>
+            </form>
+        </div>
     </div>
 
 
